58dfea66eddacb0e86bd5833da9a1bd4bc29e140
Vitaly <onebeattrue@gmail.com>
2024-04-28 02:19:51 +0300

    Научился делать отметки

--- app/src/main/java/edu/mirea/onebeattrue/samsunghack/presentation/main/map/YandexMapActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/samsunghack/presentation/main/map/YandexMapActivity.kt
@@ -3,17 +3,28 @@ package edu.mirea.onebeattrue.samsunghack.presentation.main.map
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
 import edu.mirea.onebeattrue.samsunghack.R
 
 class YandexMapActivity : ComponentActivity() {
 
     private lateinit var mapView: MapView
+
+    private val placemarkTapListener = MapObjectTapListener { _, point ->
+        Toast.makeText(
+            this,
+            "Tapped the point (${point.longitude}, ${point.latitude})",
+            Toast.LENGTH_SHORT
+        ).show()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +40,14 @@ class YandexMapActivity : ComponentActivity() {
                 /* tilt = */ 30.0f
             )
         )
+
+        val imageProvider = ImageProvider.fromResource(this, R.drawable.placemark_icon)
+        val placemark = mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+            geometry = Point(59.935493, 30.327392)
+            setIcon(imageProvider)
+        }
+
+        placemark.addTapListener(placemarkTapListener)
     }
 
     override fun onStart() {
